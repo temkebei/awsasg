9a17fc7945dd9f9d29e4b067df594abb1b083e1e
temkebei <35108092+temkebei@users.noreply.github.com>
2021-08-14 10:17:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 data "template_file" "test" {
   template = <<EOF
-        #!/bin/bash
-        sudo su
-        yum -y install httpd
-        echo "<p> Hello My web server is running! </p>" >> /var/www/html/index.html
-        sudo systemctl enable httpd
-        sudo systemctl start httpd
-    EOF
+             #!/bin/bash
+             sudo su
+             yum -y install httpd
+             echo "<p> Hello My web server is running! </p>" >> /var/www/html/index.html
+             sudo systemctl enable httpd
+             sudo systemctl start httpd
+             EOF
 }
 
 resource "aws_launch_template" "foobar" {
